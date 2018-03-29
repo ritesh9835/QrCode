0dd6c574f9c9ebcba57a6983fc668979cf2dfbb3
Ritesh-Lenovo <Ritesh-Lenovo@RITESH>
2018-03-29 12:41:23 +0530

    First code push

---
+++ QRcodeDemo/QRcodeDemo/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using QRcodeDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+
+namespace QRcodeDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Generate(QRCodeModel qrcode)
+        {
+            try
+            {
+                qrcode.QRCodeImagePath = GenerateQRCode(qrcode.QRCodeText);
+                ViewBag.Message = "QR Code Created successfully";
+            }
+            catch (Exception ex)
+            {
+                //catch exception if there is any
+            }
+            return View("Index", qrcode);
+        }
+
+        private string GenerateQRCode(string qrcodeText)
+        {
+            string folderPath = "~/Images/";
+            string imagePath = "~/Images/QrCode.jpg";
+            // If the directory doesn't exist then create it.
+            if (!Directory.Exists(Server.MapPath(folderPath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(folderPath));
+            }
+
+            var barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            var result = barcodeWriter.Write(qrcodeText);
+
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeBitmap = new Bitmap(result);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(barcodePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+            return imagePath;
+        }
+
+        public ActionResult Read()
+        {
+            return View(ReadQRCode());
+        }
+
+        private QRCodeModel ReadQRCode()
+        {
+            QRCodeModel barcodeModel = new QRCodeModel();
+            string barcodeText = "";
+            string imagePath = "~/Images/QrCode.jpg";
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeReader = new BarcodeReader();
+
+            var result = barcodeReader.Decode(new Bitmap(barcodePath));
+            if (result != null)
+            {
+                barcodeText = result.Text;
+            }
+            return new QRCodeModel() { QRCodeText = barcodeText, QRCodeImagePath = imagePath };
+        }
+    }
+}
---
+++ QRcodeDemo/QRcodeDemo/Models/QRCodeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QRcodeDemo.Models
+{
+    public class QRCodeModel
+    {
+        [Display(Name = "QRCode Text")]
+        public string QRCodeText { get; set; }
+        [Display(Name = "QRCode Image")]
+        public string QRCodeImagePath { get; set; }
+    }
+}
